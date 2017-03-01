1c9d0eb184ed21422db8e8c4f032bb0c2c9908c9
buyi <1050095062@qq.com>
2017-03-01 16:15:20 +0800

    Add the pose topic, and find that the real translation is five larger than the estimate

--- ORB_SLAM2_modified/Examples/ROS/ORB-SLAM2_DENSE/src/ros_rgbd.cc
+++ ORB_SLAM2_modified/Examples/ROS/ORB-SLAM2_DENSE/src/ros_rgbd.cc
@@ -39,14 +39,22 @@
 
 #include"../ORB_SLAM2_modified/include/System.h"
 #include"../ORB_SLAM2_modified/include/pointcloudmapping.h"
+#include <tf/transform_broadcaster.h>
 
 using namespace std;
 
 pcl::PointCloud<pcl::PointXYZRGBA> pcl_cloud;
 ros::Publisher pclPoint_pub;
-image_transport::Publisher CamPose_Pub;
+ros::Publisher CamPose_Pub;
 sensor_msgs::PointCloud2 pcl_point;
 cv::Mat Camerpose;
+tf::Transform orb_slam;
+std::vector<float> Pose_quat(4);
+std::vector<float> Pose_trans(3);
+tf::TransformBroadcaster * orb_slam_broadcaster;
+geometry_msgs::PoseStamped Cam_Pose;
+
+void Pub_CamPose(cv::Mat &pose);
 
 class ImageGrabber
 {
@@ -84,10 +92,9 @@ int main(int argc, char **argv)
     message_filters::Synchronizer<sync_pol> sync(sync_pol(10), rgb_sub,depth_sub);
     sync.registerCallback(boost::bind(&ImageGrabber::GrabRGBD,&igb,_1,_2));
 	pclPoint_pub = nh.advertise<sensor_msgs::PointCloud2>("/pclPoint_out",10);
-	CamPose_Pub = it.advertise("/CamPose",1);
+	CamPose_Pub = nh.advertise<geometry_msgs::PoseStamped>("/CamPose",1);
+	
     ros::spin();
-	
-	
     // Stop all threads
     SLAM.Shutdown();
 
@@ -132,20 +139,38 @@ void ImageGrabber::GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const senso
 	//利用cvbridge将Mat转为sensor_sensor_msgs
 	sensor_msgs::ImagePtr Cam_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", Camerpose).toImageMsg();
 	pclPoint_pub.publish(pcl_point);	
-	CamPose_Pub.publish(Cam_msg);
-	
+	Pub_CamPose(Camerpose);
 }
 
-// void Pub_CamPose(cv::Mat &pose)
-// {
-// 	cv::Mat Rwc(3,3,CV_32F);
-// 	cv::Mat twc(3,1,CV_32F);
-// 	
-// 	Rwc = pose.rowRange(0,3).colRange(0,3).t();
-// 	twc = -Rwc*pose.rowRange(0,3).col(3);
-// 	
-// 	
-// }
-
-
+void Pub_CamPose(cv::Mat &pose)
+{
+	cv::Mat Rwc(3,3,CV_32F);
+	cv::Mat twc(3,1,CV_32F);
+	Eigen::Matrix<double,3,3> rotationMat;
+	orb_slam_broadcaster = new tf::TransformBroadcaster;
+	
+	Rwc = pose.rowRange(0,3).colRange(0,3).t();
+	twc = -Rwc*pose.rowRange(0,3).col(3);
+	
+	rotationMat << Rwc.at<float>(0,0), Rwc.at<float>(0,1), Rwc.at<float>(0,2),
+				   Rwc.at<float>(1,0), Rwc.at<float>(1,1), Rwc.at<float>(1,2),
+				   Rwc.at<float>(2,0), Rwc.at<float>(2,1), Rwc.at<float>(2,2);
+	Eigen::Quaterniond Q(rotationMat);
+	Pose_quat[0] = Q.x(); Pose_quat[1] = Q.y();
+	Pose_quat[2] = Q.z(); Pose_quat[3] = Q.w();
+	
+	Pose_trans[0] = twc.at<float>(0);
+	Pose_trans[1] = twc.at<float>(1);
+	Pose_trans[2] = twc.at<float>(2);
+	
+	orb_slam.setOrigin(tf::Vector3(Pose_trans[2], -Pose_trans[0], -Pose_trans[1]));
+	orb_slam.setRotation(tf::Quaternion(Q.z(), -Q.x(), -Q.y(), Q.w()));
+	orb_slam_broadcaster->sendTransform(tf::StampedTransform(orb_slam, ros::Time::now(), "world", "orb_slam"));
+	
+	Cam_Pose.header.stamp = ros::Time::now();
+	Cam_Pose.header.frame_id = "orb_slam";
+	tf::pointTFToMsg(orb_slam.getOrigin(), Cam_Pose.pose.position);
+	tf::quaternionTFToMsg(orb_slam.getRotation(), Cam_Pose.pose.orientation);
+	CamPose_Pub.publish(Cam_Pose);
+}
 
