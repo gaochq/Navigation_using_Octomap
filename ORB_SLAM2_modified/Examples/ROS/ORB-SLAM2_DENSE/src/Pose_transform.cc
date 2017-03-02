444552f6f676adfbc16854c3d66c05cbb2258696
buyi <1050095062@qq.com>
2017-03-02 19:26:24 +0800

    Add a new node to send the transform and publish the pose

--- ORB_SLAM2_modified/Examples/ROS/ORB-SLAM2_DENSE/src/Pose_transform.cc
+++ ORB_SLAM2_modified/Examples/ROS/ORB-SLAM2_DENSE/src/Pose_transform.cc
@@ -1,85 +1,98 @@
-#include <ros/ros.h>  
-#include <visualization_msgs/Marker.h>  
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<chrono>
 
-int main( int argc, char** argv )  
-{  
-  ros::init(argc, argv, "basic_shapes");  
-  ros::NodeHandle n;  
-  ros::Rate r(1);  
-  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);  
-  
-  // 初始化形状类型为CUBE  
-  uint32_t shape = visualization_msgs::Marker::CUBE;  
-    
-  while (ros::ok())  
-  {  
-    visualization_msgs::Marker marker;  
-    // 设置坐标系ID和时间戳。参考TF turtorials  
-    marker.header.frame_id = "/my_frame";  
-    marker.header.stamp = ros::Time::now();  
-  
-    // <span style="font-family: Consolas, 'Courier New', Courier, mono, serif; font-size: 12px; line-height: 18px;">为标志物设置名称空间和独一无二的ID</span>  
-    // <span style="font-family: Consolas, 'Courier New', Courier, mono, serif; font-size: 12px; line-height: 18px; background-color: rgb(248, 248, 248);">所有的标志物设置相同的名称空间，而且id会覆盖旧的。</span>  
-    marker.ns = "basic_shapes";  
-    marker.id = 0;  
-  
-    // 设置标志物类型 Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER  
-    marker.type = shape;  
-  
-    // 设置标志物的行为（action）   Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)  
-    marker.action = visualization_msgs::Marker::ADD;  
-  
-    // 设置标志物的姿态.  This is a full 6DOF pose relative to the frame/time specified in the header  
-    marker.pose.position.x = 0;  
-    marker.pose.position.y = 0;  
-    marker.pose.position.z = 0;  
-    marker.pose.orientation.x = 0.0;  
-    marker.pose.orientation.y = 0.0;  
-    marker.pose.orientation.z = 0.0;  
-    marker.pose.orientation.w = 1.0;  
-  
-    // <span style="font-family: Consolas, 'Courier New', Courier, mono, serif; font-size: 12px; line-height: 18px;">设置标记物的尺度</span> -- 1x1x1 here means 1m on a side  
-    marker.scale.x = 1.0;  
-    marker.scale.y = 1.0;  
-    marker.scale.z = 1.0;  
-  
-    // 设置颜色 -- be sure to set alpha to something non-zero!  
-    marker.color.r = 0.0f;  
-    marker.color.g = 1.0f;  
-    marker.color.b = 0.0f;  
-    marker.color.a = 1.0;  
-  
-    marker.lifetime = ros::Duration();  
-  
-    // Publish the marker  
-    while (marker_pub.getNumSubscribers() < 1)  
-    {  
-      if (!ros::ok())  
-      {  
-        return 0;  
-      }  
-      ROS_WARN_ONCE("Please create a subscriber to the marker");  
-      sleep(1);  
-    }  
-    marker_pub.publish(marker);  
-  
-    // Cycle between different shapes  
-    switch (shape)  
-    {  
-    case visualization_msgs::Marker::CUBE:  
-      shape = visualization_msgs::Marker::SPHERE;  
-      break;  
-    case visualization_msgs::Marker::SPHERE:  
-      shape = visualization_msgs::Marker::ARROW;  
-      break;  
-    case visualization_msgs::Marker::ARROW:  
-      shape = visualization_msgs::Marker::CYLINDER;  
-      break;  
-    case visualization_msgs::Marker::CYLINDER:  
-      shape = visualization_msgs::Marker::CUBE;  
-      break;  
-    }  
-  
-    r.sleep();  
-  }  
-}  
+#include<ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include<opencv2/core/core.hpp>
+#include <image_transport/image_transport.h>
+#include <System.h>
+#include"../ORB_SLAM2_modified/include/System.h"
+#include"../ORB_SLAM2_modified/include/pointcloudmapping.h"
+#include <tf/transform_broadcaster.h>
+
+
+tf::Transform orb_slam;
+std::vector<float> Pose_quat(4);
+std::vector<float> Pose_trans(3);
+tf::TransformBroadcaster * orb_slam_broadcaster;
+geometry_msgs::PoseStamped Cam_Pose;
+ros::Publisher CamPose_Pub;
+cv::Mat Camera_Pose;
+
+void Pub_CamPose(cv::Mat &pose);
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+	try
+	{
+		Camera_Pose = cv_bridge::toCvShare(msg, "32FC1")->image.clone();
+	}
+	catch (cv_bridge::Exception& e)
+	{
+		ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+	}
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "offb_vel");
+    ros::NodeHandle nh;
+	image_transport::ImageTransport it(nh);
+	image_transport::Subscriber sub = it.subscribe("/camera/Tcw", 1, imageCallback);
+	CamPose_Pub = nh.advertise<geometry_msgs::PoseStamped>("/Camera_Pose",1);
+    ros::Rate loop_rate(50);
+	
+    while(ros::ok())
+    {
+		Pub_CamPose(Camera_Pose);
+//		ROS_INFO("size;%d",Camera_Pose.size());
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
+}
+
+void Pub_CamPose(cv::Mat &pose)
+{
+	cv::Mat Rwc(3,3,CV_32F);
+	cv::Mat twc(3,1,CV_32F);
+	Eigen::Matrix<double,3,3> rotationMat;
+	orb_slam_broadcaster = new tf::TransformBroadcaster;
+	if(pose.dims<2)
+	{
+		 ROS_INFO("Waiting fot data");
+	}
+	else
+	{
+		Rwc = pose.rowRange(0,3).colRange(0,3).t();
+		twc = -Rwc*pose.rowRange(0,3).col(3);
+		
+		rotationMat << Rwc.at<float>(0,0), Rwc.at<float>(0,1), Rwc.at<float>(0,2),
+					Rwc.at<float>(1,0), Rwc.at<float>(1,1), Rwc.at<float>(1,2),
+					Rwc.at<float>(2,0), Rwc.at<float>(2,1), Rwc.at<float>(2,2);
+		Eigen::Quaterniond Q(rotationMat);
+		Pose_quat[0] = Q.x(); Pose_quat[1] = Q.y();
+		Pose_quat[2] = Q.z(); Pose_quat[3] = Q.w();
+		
+		Pose_trans[0] = twc.at<float>(0);
+		Pose_trans[1] = twc.at<float>(1);
+		Pose_trans[2] = twc.at<float>(2);
+		
+		orb_slam.setOrigin(tf::Vector3(Pose_trans[2], -Pose_trans[0], -Pose_trans[1]));
+		orb_slam.setRotation(tf::Quaternion(Q.z(), -Q.x(), -Q.y(), Q.w()));
+		orb_slam_broadcaster->sendTransform(tf::StampedTransform(orb_slam, ros::Time::now(), "world", "base_link"));
+		
+		Cam_Pose.header.stamp = ros::Time::now();
+		Cam_Pose.header.frame_id = "orb_slam";
+		tf::pointTFToMsg(orb_slam.getOrigin(), Cam_Pose.pose.position);
+		tf::quaternionTFToMsg(orb_slam.getRotation(), Cam_Pose.pose.orientation);
+		CamPose_Pub.publish(Cam_Pose);
+	}
+}
