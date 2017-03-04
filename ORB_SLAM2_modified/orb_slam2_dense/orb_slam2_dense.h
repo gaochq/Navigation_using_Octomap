731af20786a4c17516359acc58eb81f45c3ead88
buyi <1050095062@qq.com>
2017-03-04 17:25:25 +0800

    Add the topic to publish the Pose and transform ———version2.0

---
+++ ORB_SLAM2_modified/orb_slam2_dense/orb_slam2_dense.h
@@ -0,0 +1,14 @@
+#ifndef orb_slam2_dense_H
+#define orb_slam2_dense_H
+
+#include <QtGui/QMainWindow>
+
+class orb_slam2_dense : public QMainWindow
+{
+Q_OBJECT
+public:
+    orb_slam2_dense();
+    virtual ~orb_slam2_dense();
+};
+
+#endif // orb_slam2_dense_H
